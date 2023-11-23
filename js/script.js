51d95f9ec3f48476765c2822d326a2a4603e0d90
Valery <kindness2001@mail.ru>
2023-11-23 21:38:36 +0300

    filmsl

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-console.log(1)
-console.log(1)
+const numberOfFilms = +prompt('How many you watch films?', '')
+
+console.log(numberOfFilms)
+
+
+const personalMovieDB={
+    count: numberOfFilms,
+    movies: {
+        
+    },
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+const lastFilm=prompt('last watch film','')
+const ocenka =prompt('ocenka filma','')
+
+personalMovieDB.movies[lastFilm]=ocenka
+
+console.log(personalMovieDB)
