19611f1a3cd0a1072d2eae86f8ca69ec86db2af9
Valery <kindness2001@mail.ru>
2023-11-25 16:08:31 +0300

    application Film:Raiting

--- js/script.js
+++ js/script.js
@@ -5,17 +5,16 @@ console.log(numberOfFilms)
 
 const personalMovieDB={
     count: numberOfFilms,
-    movies: {
-        
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 }
-
+11
 const lastFilm=prompt('last watch film','')
-const ocenka =prompt('ocenka filma','')
+const rating =prompt('rating film','')
 
-personalMovieDB.movies[lastFilm]=ocenka
+personalMovieDB.movies[lastFilm]=rating
+
 
 console.log(personalMovieDB)
